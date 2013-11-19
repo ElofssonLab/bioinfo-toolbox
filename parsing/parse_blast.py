ccec7e4dcbe827d706fcf62b51cfb7d27caf9e60
Mirco Michel <mirco.michel@scilifelab.se>
2013-11-19 17:51:14 +0100

    Small fix in parse_blast.py.

--- parsing/parse_blast.py
+++ parsing/parse_blast.py
@@ -18,7 +18,8 @@ def get_scores(bfile):
         # found new entry: fininsh old and create new
         if line.startswith('>'):
             if ID:
-                score_tup = (eval, ident, posit, score, len)
+                #score_tup = (eval, ident, posit, score, len)
+                score_tup = (eval, ident, score, len)
                 result_dict[ID] = score_tup
             ID = line.split(' ')[0]
             ID = ID[1:]
@@ -29,9 +30,13 @@ def get_scores(bfile):
             eval = line_arr[1].strip().split(' ')[2]
         elif line.startswith('Identities = '):
             line_arr = line.split(',')
-            ident = float(line_arr[0].strip().split(' ')[3][1:-2])
+            ident = float(line_arr[0].strip().split(' ')[3][1:-2]) / 100.0
             posit = float(line_arr[1].strip().split(' ')[3][1:-2])
             len = int(line_arr[0].split('/')[1].split(' ')[0])
+
+    # add last entry
+    score_tup = (eval, ident, score, len)
+    result_dict[ID] = score_tup
 
     bfile.close()
     return result_dict
