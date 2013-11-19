86ef6d52d848ab4b492999550e2878b73b99e2b3
Mirco Michel <mirco.michel@scilifelab.se>
2013-11-19 11:28:01 +0100

    Add homology search parsing. Gives you all necessary scores from hhsearch, jackhammer (TODO) and blast outputs.

---
+++ parsing/parse_blast.py
@@ -0,0 +1,43 @@
+import sys
+
+
+def get_scores(bfile):
+
+    result_dict = {}
+    ID = ''
+    eval = -1.0
+    score = -1.0
+    ident = -1.0
+    posit = -1.0
+    len = -1
+    
+    for line in bfile:
+       
+        line = line.strip()
+
+        # found new entry: fininsh old and create new
+        if line.startswith('>'):
+            if ID:
+                score_tup = (eval, ident, posit, score, len)
+                result_dict[ID] = score_tup
+            ID = line.split(' ')[0]
+            ID = ID[1:]
+
+        elif line.startswith('Score = '):
+            line_arr = line.split(',')
+            score = float(line_arr[0].strip().split(' ')[-3])
+            eval = line_arr[1].strip().split(' ')[2]
+        elif line.startswith('Identities = '):
+            line_arr = line.split(',')
+            ident = float(line_arr[0].strip().split(' ')[3][1:-2])
+            posit = float(line_arr[1].strip().split(' ')[3][1:-2])
+            len = int(line_arr[0].split('/')[1].split(' ')[0])
+
+    bfile.close()
+    return result_dict
+
+
+if __name__ == '__main__':
+
+    bfile = open(sys.argv[1], 'r')
+    print get_scores(bfile)
---
+++ parsing/parse_hhsearch.py
@@ -0,0 +1,69 @@
+import sys
+
+
+def get_scores(f):
+    
+    result_dict = {}
+    ID = ''
+    eval = -1.0
+    score = -1.0
+    ident = -1.0
+    len = -1
+    
+    in_hit = False
+
+    for line in f:
+       
+        line = line.strip()
+
+        # look for hit list start
+        if line.startswith('No Hit'):
+            in_hit = True
+            continue
+
+        # end of hit list        
+        if not line and in_hit:
+            in_hit = False
+            ID = ''
+
+        # parse hit list
+        if in_hit:
+            # get rid of 'HMM' column
+            line_arr_tmp = line.split('(')
+            line_tmp = ''.join(line_arr_tmp[:-1])
+            line_arr = line_tmp.split()
+            #line_arr = line.split()
+            ID = line_arr[1]
+            eval = line_arr[-7]
+            score = float(line_arr[-5])
+            len = int(line_arr[-3])
+            # the only thing missing is ident (default: -1)
+            result_dict[ID] = (eval, ident, score, len)
+
+        # parse verbose result list to get the identities
+        if line.startswith('>'):
+            if ID:
+                score_lis = list(result_dict[ID])
+                score_lis[1] = ident
+                result_dict[ID] = tuple(score_lis)
+                #print score_lis
+            ID = line.split(' ')[0]
+            ID = ID[1:]
+            #print ID
+        elif line.startswith('Probab='):
+            line_arr = line.split()
+            ident = float(line_arr[4].strip().split('=')[1][:-1]) / 100.0
+
+    # update last entry
+    score_lis = list(result_dict[ID])
+    score_lis[1] = ident
+    result_dict[ID] = tuple(score_lis)
+
+    f.close()
+    return result_dict
+
+
+if __name__ == '__main__':
+
+    bfile = open(sys.argv[1], 'r')
+    print get_scores(bfile)
