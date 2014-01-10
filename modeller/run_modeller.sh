c08af2fb548cdce75fecac2e9a22e557f85f9023
Mirco Michel <mirco.michel@scilifelab.se>
2014-01-10 09:41:30 +0100

    add modeller free modelling attempt

--- modeller/run_modeller.sh
+++ modeller/run_modeller.sh
@@ -7,5 +7,6 @@ ID=$1
 SEQFILE=$2
 TEMPL_ID=$3
 TEMPL_PDB=$4
-/home/x_mirmi/bin/modeller9.12/bin/modpy.sh python /home/x_mirmi/bioinfo-toolbox/modeller/align_to_template.py ${ID} ${SEQFILE} ${TEMPL_ID} ${TEMPL_PDB} ${ID}_${TEMPL_ID}.ali
+CHAIN=$5
+/home/x_mirmi/bin/modeller9.12/bin/modpy.sh python /home/x_mirmi/bioinfo-toolbox/modeller/align_to_template.py ${ID} ${SEQFILE} ${TEMPL_ID} ${TEMPL_PDB} ${CHAIN} ${ID}_${TEMPL_ID}.ali
 /home/x_mirmi/bin/modeller9.12/bin/modpy.sh python /home/x_mirmi/bioinfo-toolbox/modeller/model_simple.py ${ID}_${TEMPL_ID}.ali ${ID} ${TEMPL_ID}
