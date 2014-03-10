392c1418484c8277dcd917f21f6d06c3e96d778c
Mirco Michel <mirco.michel@scilifelab.se>
2014-03-10 14:18:56 +0100

    Add optional prefix.

--- parsing/parse_proq2_out.py
+++ parsing/parse_proq2_out.py
@@ -1,12 +1,14 @@
 import sys
 
-def parse(f):
+def parse(f, prefix=""):
     result = []
     f.readline() # skip line header
     for l in f:
         l = l.strip()
         l_lst = l.split()
         map(float, l_lst[1:-1])
+        new_tag = prefix + l_lst[-1]
+        l_lst[-1] = new_tag
         result.append(l_lst[1:])
     # sort by overall score
     result.sort(key=lambda itm: itm[0], reverse=True)
@@ -14,6 +16,10 @@ def parse(f):
 
 
 if __name__ == "__main__":
-    result = parse(open(sys.argv[1], 'r'))
+    if len(sys.argv) == 3:
+        prefix = sys.argv[2]
+    else:
+        prefix = ""
+    result = parse(open(sys.argv[1], 'r'), prefix)
     for sc in result:
         print ' '.join(sc)
