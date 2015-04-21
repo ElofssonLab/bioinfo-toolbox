ebe58f23994696be0e568dfc217d88659e9fdeca
Arne Elofsson <arne@bioinfo.se>
2015-04-21 22:22:19 +0200

    hej

--- arne/RNAfold/makeFACSplots.R
+++ arne/RNAfold/makeFACSplots.R
@@ -46,7 +46,10 @@ pdf("araH-GC.pdf")
 #vioplot(  araH.3, araH.4 , araH.5 ,araH.6, araH.7,araH.8,araH.9,araH.10,
 #        names=c("20.0%","26.7%","33.3%","40.0%","46.7%","53.3%","60.0%","66.7%"),
 #        col="grey")
-vioplot(   araH.4 , araH.5 ,araH.6, araH.7,araH.8,araH.9,araH.10,
+#vioplot(   araH.4 , araH.5 ,araH.6, araH.7,araH.8,araH.9,araH.10,
+#        names=c("26.7%","33.3%","40.0%","46.7%","53.3%","60.0%","66.7%"),
+#        col="grey")
+boxplot(   araH.4 , araH.5 ,araH.6, araH.7,araH.8,araH.9,araH.10,
         names=c("26.7%","33.3%","40.0%","46.7%","53.3%","60.0%","66.7%"),
         col="grey")
 title("araH %GC")
@@ -63,7 +66,11 @@ araH.codon2.35<-araH.DGsort$V3[araH.DGsort$V7==35]
 araH.codon2.36<-araH.DGsort$V3[araH.DGsort$V7==36]
 araH.codon2.57<-araH.DGsort$V3[araH.DGsort$V7==57]
 araH.codon2.58<-araH.DGsort$V3[araH.DGsort$V7==58]
-vioplot(   araH.codon1.12,araH.codon1.44,
+#vioplot(   araH.codon1.12,araH.codon1.44,
+#        araH.codon2.33,araH.codon2.34,araH.codon2.35,araH.codon2.36,araH.codon2.57,araH.codon2.58,
+#        names=c("1:ATG","1:ACG","2:TCT","2:TCC","2:TCA","2:TCG","2:AGT","2:AGC"),
+#        col="grey")
+boxplot(   araH.codon1.12,araH.codon1.44,
         araH.codon2.33,araH.codon2.34,araH.codon2.35,araH.codon2.36,araH.codon2.57,araH.codon2.58,
         names=c("1:ATG","1:ACG","2:TCT","2:TCC","2:TCA","2:TCG","2:AGT","2:AGC"),
         col="grey")
@@ -111,7 +118,10 @@ pdf("narK-GC.pdf")
 #vioplot(  narK.3, narK.4 , narK.5 ,narK.6, narK.7,narK.8,narK.9,narK.10,narK.11,
 #        names=c("20.0%","26.7%","33.3%","40.0%","46.7%","53.3%","60.0%","66.7%","73.3%"),
 #        col="grey")
-vioplot(   narK.4 , narK.5 ,narK.6, narK.7,narK.8,narK.9,narK.10,
+#vioplot(   narK.4 , narK.5 ,narK.6, narK.7,narK.8,narK.9,narK.10,
+#        names=c("26.7%","33.3%","40.0%","46.7%","53.3%","60.0%","66.7%"),
+#        col="grey")
+boxplot(   narK.4 , narK.5 ,narK.6, narK.7,narK.8,narK.9,narK.10,
         names=c("26.7%","33.3%","40.0%","46.7%","53.3%","60.0%","66.7%"),
         col="grey")
 title("narK %GC")
@@ -130,7 +140,11 @@ narK.codon2.21<-narK.DGsort$V3[narK.DGsort$V7==21]
 narK.codon2.22<-narK.DGsort$V3[narK.DGsort$V7==22]
 
 
-vioplot( narK.codon1.33,narK.codon1.34,narK.codon1.35,narK.codon1.36,narK.codon1.57,narK.codon1.58,
+#vioplot( narK.codon1.33,narK.codon1.34,narK.codon1.35,narK.codon1.36,narK.codon1.57,narK.codon1.58,
+#        narK.codon2.21,narK.codon2.22,
+#        names=c("1:TCT","1:TCC","1:TCA","1:TCG","1:AGT","1:AGC","2:CAT","2:CAC"),
+#        col="grey")
+boxplot( narK.codon1.33,narK.codon1.34,narK.codon1.35,narK.codon1.36,narK.codon1.57,narK.codon1.58,
         narK.codon2.21,narK.codon2.22,
         names=c("1:TCT","1:TCC","1:TCA","1:TCG","1:AGT","1:AGC","2:CAT","2:CAC"),
         col="grey")
