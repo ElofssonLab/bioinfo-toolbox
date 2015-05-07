993773acdf5dae76bca3f8b91a437cbe10cc6df2
Arne Elofsson <arne@bioinfo.se>
2015-05-07 11:30:46 +0200

    By syncing

--- arne/PfamProteomes/some_plots.R
+++ arne/PfamProteomes/some_plots.R
@@ -22,8 +22,6 @@ dat$Pfam_Meff[is.na(dat$Pfam_Meff)] <- 1
 
 # fraction of residues with Meff>100 among those that have a valid
 # Meff value
-length(which(dat$Pfam_Meff > 100))/length(which(dat$Pfam_Meff != -1))
-length(which(dat$Pfam_Meff > 100))/length(dat$Pfam_Meff)
 
 
 # Pfam coverage
@@ -136,5 +134,112 @@ lines(TMvar,TMMeff200,col="red")
 lines(TMvar,TMMeff500,col="green")
 lines(TMvar,TMMeff1000,col="grey")
 dev.off()
+
+
+# Pie Charts
+
+# Fraction of residues with Pfam-coverage and Meff coverage=NULL
+coverage=NULL
+labels=NULL
+coverage[1]=length(which(dat$Pfam_ID == ""))
+labels[1]="NoPfam"
+coverage[2]=length(which(dat$Pfam_Meff > 1000))
+labels[2]=">1000"
+coverage[3]=length(which(dat$Pfam_Meff > 500 & dat$Pfam_Meff <= 1000))
+labels[3]=">500"
+coverage[4]=length(which(dat$Pfam_Meff > 200 & dat$Pfam_Meff <= 500))
+labels[4]=">200"
+coverage[5]=length(which(dat$Pfam_Meff > 100 & dat$Pfam_Meff <= 200))
+labels[5]=">100"
+coverage[6]=length(which(dat$Pfam_ID != "" & dat$Pfam_Meff))
+labels[6]="Pfam"
+pct <- round(coverage/sum(coverage)*100)
+outfile=paste(genome,"-coverage.pdf",sep="")
+pdf(outfile)
+pie(coverage,labels = labels, col=rainbow(length(labels)),main=genome)
+dev.off()
+
+# FOr ordered and disordered residues
+covorder=NULL
+labels=NULL
+colors=NULL
+covorder[1]=length(which(dat$Pfam_ID == ""))
+labels[1]="NoPfam"
+colors[1]="cadetblue1"
+covorder[6]=length(which(dat$Pfam_Meff > 1000 & dat$Disorder<0.5))
+labels[6]=">1000"
+colors[6]="blue4"
+covorder[5]=length(which(dat$Pfam_Meff > 500 & dat$Pfam_Meff <= 1000 & dat$Disorder<0.5))
+labels[5]=">500"
+colors[5]="blue3"
+covorder[4]=length(which(dat$Pfam_Meff > 200 & dat$Pfam_Meff <= 500 & dat$Disorder<0.5))
+labels[4]=">200"
+colors[4]="blue2"
+covorder[3]=length(which(dat$Pfam_Meff > 100 & dat$Pfam_Meff <= 200 & dat$Disorder<0.5))
+labels[3]=">100"
+colors[3]="blue1"
+covorder[2]=length(which(dat$Pfam_ID != "" & dat$Pfam_Meff & dat$Disorder<0.5))
+labels[2]="Pfam"
+colors[2]="cadetblue2"
+pct <- round(covorder/sum(covorder)*100)
+labels <- paste(labels,pct)
+labels <- paste(labels,"%",sep="")
+outfile=paste(genome,"-pie-order.pdf",sep="")
+pdf(outfile)
+pie(covorder,labels = labels, col=rainbow(length(labels)),main=genome)
+dev.off()
+
+covdisorder=NULL
+dislabels=NULL
+covdisorder[1]=length(which(dat$Pfam_ID == ""))
+covorder[7]=length(which(dat$Pfam_ID == ""))
+dislabels[1]="NoPfam"
+labels[7]="Diso-NoPfam"
+colors[7]="lightpink"
+
+covdisorder[6]=length(which(dat$Pfam_Meff > 1000 & dat$Disorder>=0.5))
+covorder[12]=length(which(dat$Pfam_Meff > 1000 & dat$Disorder>=0.5))
+dislabels[6]=">1000"
+labels[12]=">1000 diso"
+colors[12]="red"
+covdisorder[5]=length(which(dat$Pfam_Meff > 500 & dat$Pfam_Meff <= 1000 & dat$Disorder>=0.5))
+covorder[11]=length(which(dat$Pfam_Meff > 500 & dat$Pfam_Meff <= 1000 & dat$Disorder>=0.5))
+dislabels[5]=">500"
+labels[11]=">500 diso"
+colors[9]="deepink3"
+covdisorder[4]=length(which(dat$Pfam_Meff > 200 & dat$Pfam_Meff <= 500 & dat$Disorder>=0.5))
+covorder[10]=length(which(dat$Pfam_Meff > 200 & dat$Pfam_Meff <= 500 & dat$Disorder>=0.5))
+dislabels[4]=">200"
+labels[10]=">200 diso"
+colors[9]="deepink2"
+covdisorder[3]=length(which(dat$Pfam_Meff > 100 & dat$Pfam_Meff <= 200 & dat$Disorder>=0.5))
+covorder[9]=length(which(dat$Pfam_Meff > 100 & dat$Pfam_Meff <= 200 & dat$Disorder>=0.5))
+dislabels[3]=">100"
+labels[9]=">100 diso"
+colors[9]="deeppink1"
+covdisorder[2]=length(which(dat$Pfam_ID != "" & dat$Pfam_Meff & dat$Disorder>=0.5))
+covorder[8]=length(which(dat$Pfam_ID != "" & dat$Pfam_Meff & dat$Disorder>=0.5))
+dislabels[2]="Pfam"
+labels[8]="Pfam diso"
+colors[8]="lightpink2"
+
+pct <- round(covdisorder/sum(covdisorder)*100,digits=2)
+dislabels <- paste(dislabels,pct)
+dislabels <- paste(dislabels,"%",sep="")
+outfile=paste(genome,"-pie-disorder.pdf",sep="")
+pdf(outfile)
+pie(covdisorder,labels = labels, col=rainbow(length(labels)),main=genome)
+dev.off()
+
+pct <- round(covorder/sum(covorder)*100,digits=2)
+labels <- paste(labels,pct)
+labels <- paste(labels,"%",sep="")
+outfile=paste(genome,"-pie-all.pdf",sep="")
+pdf(outfile)
+pie(covorder,labels = labels, col=colors   ,main=genome,cex=0.3)
+dev.off()
+
+
+
 }    
 #Pfam.log <- log10(dat$Pfam_Meff)
