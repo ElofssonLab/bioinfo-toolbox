e5e29beb6e607df33633b5ed2984fb80f15a0a45
Arne Elofsson at Abisko <arne@bioinfo.se>
2015-08-15 07:56:03 +0200

    By syncing

---
+++ arne/PfamProteomes/extract-seq-subdir.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import sys, getopt,re
+import os
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+#from Bio.SeqFeature import SeqFeature, FeatureLocation
+
+
+file=sys.argv[1]
+dir=sys.argv[2]
+
+# Avoid having too many sequences in a single dir..
+#subdir=
+
+# Open GenBank file
+handle = open(file, 'rU')
+
+print "opening "+ file +"\n"
+# For each record (mitochrodrial genome, in this case)...
+for record in SeqIO.parse(handle, 'fasta') :
+#   print record.name
+   # Grab the entire sequence
+#   seq = str(record.seq)
+   name= re.sub(r'\|','-',str(record.name))
+
+#   record.id=str(record.name)
+   # Look at all features for this record
+   #   for feature in record.features:
+   subdir=re.sub(r'.*\_','',name)[:3]
+#   print subdir
+
+   if (not os.path.isdir(dir + "/" +subdir)): 
+      os.mkdir(dir + "/" +subdir)
+   OutFile = open(dir + "/" + subdir + "/" + name +  '.fa', 'w')
+#   print "FILE: " + name + "\n"
+   SeqIO.write(record, OutFile, "fasta")
+   exit 
