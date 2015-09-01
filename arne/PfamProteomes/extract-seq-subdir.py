0221e1d15c41dcd1139be5ccccdf504283e27376
Arne Elofsson at Abisko <arne@bioinfo.se>
2015-09-01 06:12:01 +0200

    By syncing

--- arne/PfamProteomes/extract-seq-subdir.py
+++ arne/PfamProteomes/extract-seq-subdir.py
@@ -17,11 +17,14 @@ dir=sys.argv[2]
 
 # Open GenBank file
 handle = open(file, 'rU')
+
+infofile = open(file + ".names", 'w')
+
 count=0
 #print "opening "+ file +"\n"
 # For each record (mitochrodrial genome, in this case)...
 for record in SeqIO.parse(handle, 'fasta') :
-#   print record.name
+##   print record.name
    # Grab the entire sequence
 #   seq = str(record.seq)
    name= re.sub(r'\|','-',str(record.name))
@@ -30,14 +33,19 @@ for record in SeqIO.parse(handle, 'fasta') :
    # Look at all features for this record
    #   for feature in record.features:
    subdirA=re.sub(r'.*\_','',name)[:2]
-   subdirB=re.sub(r'.*\_','',name)[2:5]
-   subdirC=re.sub(r'.*\_','',name)[5:8]
+   subdirB=re.sub(r'.*\_','',name)[2:4]
+   subdirC=re.sub(r'.*\_','',name)[4:6]
+   subdirD=re.sub(r'.*\_','',name)[6:8]
+   subdirE=re.sub(r'.*\_','',name)[8:10]
+   subdirF=re.sub(r'.*\_','',name)[10:12]
+   subdirG=re.sub(r'.*\_','',name)[12:14]
 
 #   print subdirA,subdirB,subdirC
 
-   subdir = subdirA+"/"+subdirB+"/"+subdirC
+   subdir = subdirA+"/"+subdirB+"/"+subdirC+"/"+subdirD+"/"+subdirE+"/"+subdirF+"/"+subdirG
    if (not os.path.isdir(dir + "/" +subdir)): 
       os.makedirs(dir + "/" +subdir)
+   infofile.write(dir + "/" + subdir + "/" + name +  '.fa' + "\n")
    if (not os.path.isfile(dir + "/" + subdir + "/" + name +  '.fa')): 
       try:
          OutFile = open(dir + "/" + subdir + "/" + name +  '.fa', 'w')
@@ -48,4 +56,4 @@ for record in SeqIO.parse(handle, 'fasta') :
          print "error" + dir + "/" + subdir + "/" + name +  '.fa'
 #   else:
 #      print "skipping: " + name + "\n"
-print "Wote number of files: ",file,count
+
