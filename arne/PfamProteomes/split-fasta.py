679813e7b06efad074cc156e040211ce5ad640fa
Arne Elofsson at Abisko <arne@bioinfo.se>
2015-09-16 20:11:46 +0200

    By syncing

--- arne/PfamProteomes/split-fasta.py
+++ arne/PfamProteomes/split-fasta.py
@@ -18,18 +18,31 @@ file=sys.argv[1]
 handle = open(file, 'rU')
 
 j=0
-cutoff=100000
-#print "opening "+ file +"\n"
-# For each record (mitochrodrial genome, in this case)...
+cutoff=10000
 num=0
-OutFile = open(file + ".split-" + str(j), 'w')
+OutFile = open(file + ".split-" + str(cutoff) +"-"  + str(j) +".fasta", 'w')
+InfoFile = open(file + ".split-" + str(cutoff) +"-"  + str(j) +".names", 'w')
 for record in SeqIO.parse(handle, 'fasta') :
    # Grab the entire sequence
    num=num+1
-   if (num>cutoff):
+   if (num>=cutoff):
       OutFile.close()
-      OutFile = open(file + ".split-" + str(j), 'w')
+      InfoFile.close()
+      OutFile = open(file + ".split-" + str(cutoff) +"-"  + str(j) +".fasta", 'w')
+      InfoFile = open(file + ".split-" + str(cutoff) +"-"  + str(j) +".names", 'w')
       j=j+1
       num=0
    SeqIO.write(record, OutFile, "fasta")
+   name= record.id
+   subdirA=re.sub(r'.*\_','',name)[:2]
+   subdirB=re.sub(r'.*\_','',name)[2:4]
+   subdirC=re.sub(r'.*\_','',name)[4:6]
+   subdirD=re.sub(r'.*\_','',name)[6:8]
+   subdirE=re.sub(r'.*\_','',name)[8:10]
+   subdirF=re.sub(r'.*\_','',name)[10:12]
+   subdirG=re.sub(r'.*\_','',name)[12:14]
+   subdir = subdirA+"/"+subdirB+"/"+subdirC+"/"+subdirD+"/"+subdirE+"/"+subdirF+"/"+subdirG
+   InfoFile.write(subdir+"/"+name+".fa\n")
+   
 OutFile.close()
+InfoFile.close()
