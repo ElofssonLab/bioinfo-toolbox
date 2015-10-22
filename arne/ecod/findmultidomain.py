f00ec4237f21c2c58bb6b219cd11a531dd0ac91e
Arne Elofsson <arne@bioinfo.se>
2015-10-22 16:38:44 -0500

    By syncing

--- arne/ecod/findmultidomain.py
+++ arne/ecod/findmultidomain.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os, sys
+import os, sys, re
 import pandas as pd
 
 data_file="data/ecod.latest.domains.tsv"
@@ -8,7 +8,7 @@ data = pd.read_csv(data_file, sep='\t',na_values=['na'])
 #n = data[data['pdb'] == '4ag1']
 
 # Filter for resolution etc etc ..
-
+fold={}
 
 for p in set(data['pdb']):
     pdb=data[data['pdb'] == p]
@@ -16,10 +16,22 @@ for p in set(data['pdb']):
     for c in set(pdb['chain']):
         chain=pdb[pdb['chain'] == c]
         numdom=len(set(chain['ecod_domain_id']))
+# we only want entries with a single chain and multiple domains
         if (numchain == 1 and numdom > 1):
-            print "Domain: ",p,c,numchain,numdom
-#        for d in set(chain['ecod_domain_id']):
-#            domain=chain[chain['ecod_domain_id'] == d ]
-#                #print domain
-                # print "TEST: ",p,c,d,numchain,numdom
-
+            new=True
+            newfolds=[]
+#    check if any fold has been used before
+            for d in set(chain['ecod_domain_id']):
+                domain=chain[chain['ecod_domain_id'] == d ]
+                for f in set(domain['f_id']):
+                    (A,X,H,T)=re.split('\.',f)
+                    FOLD=A+"."+X+"."+T
+#                    print FOLD
+                    if FOLD in fold.keys():
+                        new=False
+                    newfolds.append(FOLD)
+            for f in newfolds:
+                fold[f]=f
+            if (new):
+                print "Domain: ",p,c,numchain,numdom,newfolds
+                
