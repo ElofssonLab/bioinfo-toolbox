e431f9ac6a098067c0ea5d5b7920977bf8833274
Arne Elofsson <arne@bioinfo.se>
2016-02-22 11:54:08 +0100

    By syncing

--- contacts/ArneAnalysis/makeplots.bash
+++ contacts/ArneAnalysis/makeplots.bash
@@ -3,6 +3,7 @@
 for i in "$@"
 do
     j=`basename $i .PconsC3.l5  `
+    j=`basename $j .PconsC3.l4  `
     j=`basename $j .rr`
     j=`basename $j .gdca`
     j=`basename $j .0.02.plm20`
@@ -15,13 +16,28 @@ do
     else
 	e="fasta"
     fi
+    p=' '
+    if [ -e $k-iupred-long.txt ]
+    then
+	p=$p" --iupred $k-iupred-long.txt " 
+    fi
+
+    if [ -e $j.gneff ]
+    then
+	p=$p" --meff $j.gneff  "
+    fi
     if [ -e $l.pdb ]
     then
-    ~/git/bioinfo-toolbox/contacts/ArneAnalysis/plot_contact_map.py --cutoff 0.3 --meff $j.gneff --alignment $j.trimmed  --iupred $k-iupred-long.txt  --psipred_vert $j.ss2 --pdb $l.pdb  $k.$e $j.$f
-    ~/git/bioinfo-toolbox/contacts/ArneAnalysis/plot_contact_map.py --cutoff 0.3 --alignment $j.trimmed  --iupred $k-iupred-long.txt  --psipred_vert $j.ss2 --pdb $l.pdb  $k.$e $j.$f -o $j-nomeff.pdf
-    ~/git/bioinfo-toolbox/contacts/ArneAnalysis/analyse_predictions.py --cutoff 0.3 --meff $j.gneff --alignment $j.trimmed  --iupred $k-iupred-long.txt  --psipred_vert $j.ss2  --pdb $l.pdb $k.$e $j.$f
-    else
-    ~/git/bioinfo-toolbox/contacts/ArneAnalysis/plot_contact_map.py --cutoff 0.3 --meff $j.gneff --alignment $j.trimmed  --iupred $k-iupred-long.txt  --psipred_vert $j.ss2  $k.$e $j.$f
-    ~/git/bioinfo-toolbox/contacts/ArneAnalysis/analyse_predictions.py --cutoff 0.3 --meff $j.gneff --alignment $j.trimmed  --iupred $k-iupred-long.txt  --psipred_vert $j.ss2 $k.$e $j.$f
+	p=$p" --pdb $l.pdb  "
     fi
+    if [ -e $j.trimmed ]
+    then
+	p=$p" --alignment $j.trimmed  "
+    fi
+    if [ -e $j.ss2 ]
+    then
+	p=$p" --psipred_vert $j.ss2  "
+    fi
+    ~/git/bioinfo-toolbox/contacts/ArneAnalysis/plot_contact_map.py --cutoff 0.3 $p    $k.$e $j.$f
+#    ~/git/bioinfo-toolbox/contacts/ArneAnalysis/analyse_predictions.py --cutoff 0.3 $p $k.$e $j.$f
 done
