c3db8803f26a11b823a5a93caebdcd24a6727a75
Arne Elofsson <arne@bioinfo.se>
2016-03-17 20:08:26 +0100

    By syncing

--- contacts/ArneAnalysis/analyse_predictions.py
+++ contacts/ArneAnalysis/analyse_predictions.py
@@ -429,14 +429,16 @@ def contactanalysis(fasta_filename, c_filename, factor=1.0, cutoff=9999.99, th=-
             tooclose.append(score)
            
                 
-    line="Highs: %.1f %.3f% %.3f%\t average:  %.2f %.2f %.2f\t Meff: %.0f\t Diso: %.3f% \t" % (count/ref_len,mixcount/count,disocount/count,average,mixaverage,disoaverage,max_cover,fraction_disorder)
+    statline="Highs: %.1f %.3f %.3f\t average:  %.2f %.2f %.2f\t Meff: %.0f\t Diso: %.3f \t" % (count/ref_len,mixcount/count,disocount/count,average,mixaverage,disoaverage,max_cover,fraction_disorder)
+    statline="%d\t%d\t%d\t%d\t%d\t%.3f\t%.3f\t%.3f\t%.3f\n"  % ( ref_len,max_cover,(count-mixcount-disocount),mixcount,disocount,sum,mixsum,disosum,fraction_disorder)
+    statline="%d\t%.3f\t%.3f\t%.3f\t%3f\t%.3f\t%.3f\t%.3f\t%.3f\n"  % ( max_cover,count/ref_len,(count-mixcount-disocount)/(1.e-20+ref_len*(1-fraction_disorder)),mixcount/(1.e-20+2*ref_len*(1-fraction_disorder)*fraction_disorder),disocount/(1.e-20+ref_len*(fraction_disorder)),average,mixaverage,disoaverage,fraction_disorder)
     fig = plt.figure(figsize=(8, 8), dpi=96, facecolor='w')
     plt.hist((tooclose,scores), numbins,range=(0,1), histtype='bar',
              normed=(numbins,numbins), alpha=0.75,
              label=['Too_Close','Contacts'])
     plt.xlabel('Score')
     plt.ylabel('Normalized count')
-    fig.suptitle('%s\n%s\n' %  (c_filename,line))
+    fig.suptitle('%s\n%s\n' %  (c_filename,statline))
 
     
     ### Calculate reference contacts in the background if given
@@ -495,9 +497,9 @@ def contactanalysis(fasta_filename, c_filename, factor=1.0, cutoff=9999.99, th=-
            
 #        ppv='PPV: %.2f %.2f %.2f' % (float(PPVs[-1]), float(TPs[-1]), float(FPs[-1]))
         ppv='PPV: %.2f (%d)\t%.2f (%d)\t%.2f (%d) ' % (float(PPVs[-1]),len(PPVs), float(mixPPVs[-1]),len(mixPPVs), float(disoPPVs[-1]),len(disoPPVs))
-        print "STATs: %s \t %s\t%s\t" % (fasta_filename,line,ppv)
+        print "STATs: %s \t %s\t%s\t" % (fasta_filename,statline,ppv)
     else:
-        print "STATs: %s \t %s\t" % (fasta_filename,line)
+        print "STATs: %s \t %s\t" % (fasta_filename,statline)
 
 
     # We should print statistics for each residue..
