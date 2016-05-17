d2bc820615ff9921329d1dfcd7d8ca696fb65c29
Mirco Michel <mirco.michel@scilifelab.se>
2016-05-17 11:18:42 +0200

    Add Pcons scores (including conversion from s-score to distace)

--- plot_qa.py
+++ plot_qa.py
@@ -1,7 +1,19 @@
 import sys
+from math import *
 from collections import defaultdict
 
 import matplotlib.pyplot as plt
+
+
+def s2d(s):
+    d0=5
+    d=100 # for CASP we cap the distance at 100 angstroms
+    if s>0.00249: # this is the S score for 100 angstroms
+        if s>=1:
+            d=0
+        else:
+            d=sqrt(1/s-1)*d0
+    return d 
 
 
 def parse_qa(f):
@@ -9,21 +21,33 @@ def parse_qa(f):
     ignore_lst = ['PFRMAT', 'TARGET', 'AUTHOR', 'REMARK', 'MODEL', 'QMODE', 'END']
     with open(f) as qa_f:
         acc = ''
+        method = ''
         for l in qa_f:
             l_arr = l.strip().split()
             # replace unpredicted positions
             l_arr = [100 if x=='X' else x for x in l_arr]
             if l.startswith('METHOD'):
-                method = l_arr[1]
-                continue
+                if not method:
+                    method = l_arr[1]
+                    if '*' in method:
+                        method = 'Pcons'
+                    continue
+                else:
+                    continue
             if l_arr[0] in ignore_lst:
                 continue
             try:
                 float(l_arr[0])
-                qa_dict[acc] += map(float, l_arr)
+                if method == 'Pcons':
+                    qa_dict[acc] += map(s2d, map(float, l_arr))
+                else:
+                    qa_dict[acc] += map(float, l_arr)
             except ValueError:
                 acc = l_arr[0]
-                qa_dict[acc] = map(float, l_arr[2:])
+                if method == 'Pcons':
+                    qa_dict[acc] = map(s2d, map(float, l_arr[2:]))
+                else:
+                    qa_dict[acc] = map(float, l_arr[2:])
     return method, qa_dict
 
 
@@ -48,9 +72,10 @@ def plot_qa(f_lst, outf_prefix=''):
         #plt.legend(plot_lst, label_lst)
         ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=3)
         #plt.show()
-        plt.ylim(0,25)
+        plt.ylim(0,50)
         outf = '%s%s.qa.png' % (outf_prefix, acc)
         plt.savefig(outf)
+        plt.close()
 
             
             
