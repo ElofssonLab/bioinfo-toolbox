6f60de515f41886073d9ebfc5a649dac525e5343
Arne Elofsson <arne@bioinfo.se>
2017-02-23 12:30:48 +0100

    By syncing

--- arne/CONFOLD/run_confold_mem.bash
+++ arne/CONFOLD/run_confold_mem.bash
@@ -9,9 +9,22 @@ ulimit -s unlimited
 #id=$1
 #echo $id
 
-confold=/proj/bioinfo/software/confold/CONFOLD/confold.pl
-#confold=$HOME/git/bioinfo-toolbox/arne/CONFOLD/confold.pl
-l=2.5
+#confold=/proj/bioinfo/software/confold/CONFOLD/confold.pl
+#confold=$HOME/git/bioinfo-toolbox/arne/CONFOLD/confold_mem.pl
+
+if [ -s /proj/bioinfo/software/confold/CONFOLD/confold_mem.pl ]
+then
+    confold=/proj/bioinfo/software/confold/CONFOLD/confold_mem.pl
+elif  [ -s /pfs/nobackup/home/m/mircomic/confold/CONFOLD/confold_mem.pl ]
+then
+    confold=/pfs/nobackup/home/m/mircomic/confold/CONFOLD/confold_mem.pl
+else
+    confold=$HOME/git/bioinfo-toolbox/arne/CONFOLD/confold_mem.pl
+fi
+
+
+
+#l=2.5
 
 #dir=data/29.0/$id
 
@@ -24,12 +37,16 @@ seq=$1
 rr=$2
 ss=$3
 top=$4
-outdir=${rr}_confold_mem
+cutoff=$5
+mindist=$6
+maxdist=$7
+
+outdir=${rr}_${cutoff}_${mindist}_${maxdist}_confold_mem
 
 if [ ! -f $outdir/stage2/${id}.fa_model1.pdb ]; then
     rm -rf $outdir
     mkdir $outdir
-    $confold -top $top -seq $seq -rr $rr -ss $ss -o $outdir -selectrr ${l}L -stage2 1 -mcount 20 &> $outdir/${id}.log
+    $confold -top $top -seq $seq -rr $rr -ss $ss -o $outdir -selectrr $cutoff -stage2 0 -mcount 50 -mindist $mindist -maxdist $maxdist &> $outdir/${id}.log
 fi
 ls -l $outdir/stage2/${id}.fa_model1.pdb
 
