115b1dcbadeb00795e7ce3647d15952b623ed3d0
Arne Elofsson at Abisko <arne@bioinfo.se>
2017-09-12 11:33:59 +0200

    By syncing

--- parsing/extract_all_pdb_seq.py
+++ parsing/extract_all_pdb_seq.py
@@ -16,14 +16,18 @@ if (code == 'XXXX' or code == '' or code == 'xxxx' or code == '    '):
 print ":"+code+":"
 pdbfile.close()
 pdbfile = open(sys.argv[1], 'r')
+name = sys.argv[1].replace(".pdb","_")
 chains = get_all_chains(pdbfile)
 pdbfile.close()
 #print chains
 for chain in chains:
     pdbfile = open(sys.argv[1], 'r')
-    print "> " + code + "_" + chain
-    print get_atom_seq(pdbfile, chain)
+    outname=name+chain+".fa"
+    outfile = open(outname,'w')
+    outfile.write("> " + code + "_" + chain + "\n")
+    outfile.write(get_atom_seq(pdbfile, chain) + "\n")
     pdbfile.close()
+    outfile.close()
 #pdbfile = open(sys.argv[1], 'r')
 #print get_coordinates(pdbfile)
 #pdbfile.close()
