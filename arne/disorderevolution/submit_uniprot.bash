590312b13e7f0babb6d81e4fa09ba9372ee33806
Arne Elofsson at Abisko <arne@bioinfo.se>
2018-08-17 11:24:32 +0200

    changed annotation

--- arne/disorderevolution/submit_uniprot.bash
+++ arne/disorderevolution/submit_uniprot.bash
@@ -10,13 +10,13 @@ snic=SNIC2018-1-26
 time="04:00:00"
 mem="2GB"
 
-#offset=$2
-offset=0  # Maximum  number of 
+offset=$2
+#offset=0  # Maximum  number of 
 list=$1
 pos=$(($SLURM_ARRAY_TASK_ID + $offset))
 #id=`tail -n+$pos IDs_29.0_test_done_300.txt | head -n1`
 id=`tail -n+$pos $list | head -n1`
-id="./data/proteomes/UP000197915_1961136.fasta"
+#id="./data/proteomes/UP000196581_946573.fasta"
 
 
 dir="/pfs/nobackup/home/w/wbasile/annotate_uniprot_proteomes/bin/"
@@ -30,4 +30,11 @@ then
     do
 	wget -qO - https://www.uniprot.org/uniprot/${k}.xml  >>  ${outdir}/$j.xml
     done
-done
+fi
+if [ ! -s ${outdir}/${j}.txt ]
+then
+    for k in `grep \> ${id} | sed "s/|/ /g" | gawk '{print $2}'`
+    do
+	wget -qO - https://www.uniprot.org/uniprot/${k}.txt  >>  ${outdir}/$j.txt
+    done
+fi
