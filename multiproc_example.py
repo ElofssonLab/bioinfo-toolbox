b6e51b453a8f9e4e94c01287f456ca30507f0a8a
Gabriele Pozzati <gabrielepozzati@hotmail.it>
2019-10-29 14:19:18 +0100

    Add files via upload

---
+++ multiproc_example.py
@@ -0,0 +1,31 @@
+import multiprocessing as mp
+import time
+
+
+def worker(number):
+    print (str(number)+' start')
+    for n in range(0, number, 1):
+        time.sleep(2)
+        print ('wait for '+str(number))
+    print (str(number)+' done!')
+    q.put(number)
+
+def output():
+    while 1: print (str(q.get())+' is a good boi!')
+
+if __name__ == '__main__':
+
+
+    joblist = [2, 4, 6, 8]
+
+    p = mp.Process(target=output, args=())  ##### single process
+    pool = mp.Pool(processes=4)             ##### list of processes to split in a max number of parallel jobs
+    q = mp.Queue()                          ##### queue
+
+    p.start()
+
+    pool.map(worker, joblist)
+    pool.close()
+    pool.join()
+    
+    p.terminate()
