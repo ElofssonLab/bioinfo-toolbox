a77f953991dd1f5ea24a921d9c1ab090e6a509be
Aditi Shenoy <aditi@b-an01.hpc2n.umu.se>
2020-02-07 13:46:14 +0100

    test comment addded

--- merge_pdfs.sh
+++ merge_pdfs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+#test
 DIR=$1
 echo "Merging ${DIR}/*.pdf into ${DIR}/merged.pdf". This may take a while...
 gs -q -sPAPERSIZE=letter -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=${DIR}/merged.pdf `ls ${DIR}/*.pdf`
