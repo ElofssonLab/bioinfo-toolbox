2071c01f091ddf236b407754d369bcc8a1b21271
Arne Elofsson <arne@bioinfo.se>
2020-03-08 12:46:47 +0100

    By syncing

--- arne/disorderpred/dis_pred.py
+++ arne/disorderpred/dis_pred.py
@@ -21,112 +21,7 @@ import re
 import seaborn as sb
 import tensorflow as tf
 
-if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description =
-    '- Neural network build manager -',
-    formatter_class=RawTextHelpFormatter)
-    formatter_class=RawTextHelpFormatter)
-    #parser.add_argument('-t', required= True, help='path to training file list')
-    #parser.add_argument('-d', required= True, help='path to data folder')
-    parser.add_argument('-f', required= True, help='feature kind (pro, rna)')
-    #parser.add_argument('-m', required= True, help='model')
-    #parser.add_argument('-final', required= False,  help=' Use Final Model', action='store_true')
-    parser.add_argument('-gc', required= False,  help=' GC', action='store_true')
-    parser.add_argument('-gcgenomic', required= False,  help=' GCgenomic', action='store_true')
-    parser.add_argument('-kingdom', required= False,  help=' Kingdom', action='store_true')
-    ns = parser.parse_args()
-    ns = parser.parse_args()
-    if (ns.gc): ns.gc='GC'
-    else: ns.gc='noGC'
-    cutoff =0.4 # Prediction cutoff.
-    iupredcutoff =0.4 # Prediction cutoff.
-    seed = 42
-    tiny=1.e-20
-    os.environ['PYTHONHASHSEED'] = '0'
-    np.random.seed(seed)
-    random.seed(seed)
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    tf.keras.backend.set_session(tf.Session(config=config))
-    session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
-    tf.set_random_seed(seed)
-    sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
-    K.set_session(sess)
-
-    if ns.f == 'pro': feat_len=20
-    elif ns.f == 'rna': feat_len=61
-    else: sys.exit('Unknown ')
-    if ns.gc: feat_len+=1
-    if ns.gcgenomic:  feat_len+=1
-
-    
-    ##### Dataset #####
-    #print ('Loading data ...')
-    #data = h5py.File(ns.d+'formatted_data_GC.h5py','r')
-    #with open(ns.d+'mobidata_K.pickle','rb') as f:
-    #    gc = pickle.load(f)
-    #test_list = []
-    #for line in open(ns.t, 'r'): test_list.append(line.rstrip())
-
-    model = load_model(ns.m)
-
-    test_cm = {}
-    for thr in nl.thrlist: test_cm[thr] = test_cm.get(thr, {'PP':{'TP':0,'FP':0},'PN':{'TN':0,'FN':0}})
-
-# This is not complete as we have to check if we have the data for that residue as well
-    ##### Prediction #####
-    #pred = {'Name':[], 'kingdom':[], 'gc':[], 'TP':[], 'FP':[], 'FN':[], 'TN':[], 'Pred':[], 'Diso':[]}
-    pred = {}
-    i=0
-
-
-    # First we need to take a fasta file (+DNA data) and make h5py file.
-
-
-    for protein in test_list:
-            sample=np.concatenate((data[protein]['GC'],data[protein][ns.f]),axis=1)
-        else:
-            sample = np.array(data[protein][ns.f], dtype=np.float64)
-        X = sample[:,:-1].reshape(1, len(sample), len(sample[0])-1)
-        Y = sample[:,-1]
-        #print (protein,X,Y)
-        prediction = model.predict_on_batch(X)
-
-
-
-        #!/usr/bin/env python3
-
-parser = argparse.ArgumentParser(description =
-                                 '- format h5py file  -',
-                                 formatter_class=RawTextHelpFormatter)
-parser.add_argument('-f', required= True, help='Input file')
-parser.add_argument('-gc', required= False, help='GC', action='store_true')
-parser.add_argument('-rna', required= False, help='RNA', action='store_true')
-parser.add_argument('-pro', required= False, help='Protein', action='store_true')
-parser.add_argument('-gcgenomic', required= False, help='GCgenomic', action='store_true')
-parser.add_argument('-kingdom', required= False, help='Kingdom', action='store_true')
-ns = parser.parse_args()
-
-type=''
-extra=0
-if ns.pro:
-    type+="pro"
-elif ns.rna:
-    type+="rna"
-
-if ns.gc:
-    type+="_GC"
-    extra+=1
-if ns.gcgenomic:
-    type+="_GCgenomic"
-    extra+=1
-
-if ns.kingdom:
-    type+="_kingdom"
-    extra+=3
-
-    
 codons = [
 'ATA', 'ATC', 'ATT', 'ATG', 'ACA', 'ACC', 'ACG', 'ACT', 
 'AAC', 'AAT', 'AAA', 'AAG', 'AGC', 'AGT', 'AGA', 'AGG',                  
@@ -178,41 +73,139 @@ for codon in codons:
     pos += 1
 #####
 
-data_dir = "/home/arnee/git/paper_proks_vs_euks_proteins/data/"
+if __name__ == '__main__':
 
-df_reference_file = data_dir  + "df_reference.csv"
-df_reference = pd.read_csv(df_reference_file)
-taxid2gc = df_reference.set_index("TaxID").to_dict()["GC%"]
+    parser = argparse.ArgumentParser(description =
+                                         '- Running disorder prediction -',
+        formatter_class=RawTextHelpFormatter)
+    #parser.add_argument('-t', required= True, help='path to training file list')
+    #parser.add_argument('-d', required= True, help='path to data folder')
+    parser.add_argument('-f', required= True, help='feature kind (pro, rna)')
+    #parser.add_argument('-m', required= True, help='model')
+    #parser.add_argument('-final', required= False,  help=' Use Final Model', action='store_true')
+    parser.add_argument('-gc', required= False,  help=' GC', action='store_true')
+    parser.add_argument('-gcgenomic', required= False,  help=' GCgenomic', action='store_true')
+    parser.add_argument('-kingdom', required= False,  help=' Kingdom', action='store_true')
+    parser = argparse.ArgumentParser(description =
+                                 '- format h5py file  -',
+                                 formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-f', required= True, help='Input file')
+    parser.add_argument('-gc', required= False, help='GC', action='store_true')
+    parser.add_argument('-rna', required= False, help='RNA', action='store_true')
+    parser.add_argument('-pro', required= False, help='Protein', action='store_true')
+    parser.add_argument('-gcgenomic', required= False, help='GCgenomic', action='store_true')
+    parser.add_argument('-kingdom', required= False, help='Kingdom', action='store_true')
+    ns = parser.parse_args()
 
 
+    if (ns.gc): ns.gc='GC'
+    else: ns.gc='noGC'
 
-#            try:
-#                gc=float(taxid2gc[int(tax_id)])
+    cutoff =0.4 # Prediction cutoff.
+    seed = 42
+    tiny=1.e-20
+    os.environ['PYTHONHASHSEED'] = '0'
+    np.random.seed(seed)
+    random.seed(seed)
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    tf.keras.backend.set_session(tf.Session(config=config))
+    session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+    tf.set_random_seed(seed)
+    sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+    K.set_session(sess)
+
+    if ns.f == 'pro': feat_len=20
+    elif ns.f == 'rna': feat_len=61
+    else: sys.exit('Unknown ')
+    if ns.gc: feat_len+=1
+    if ns.gcgenomic:  feat_len+=1
+
+    #   First we need to create the input file from a fasta file (and
+    #   potentially other information as well)
 
 
-fasta_dir="uniprot/"
-with open(ns.f,'rb') as f:
-    data = pickle.load(f)
+    type=''
+    extra=0
+    if ns.pro:
+        type+="pro"
+    elif ns.rna:
+        type+="rna"
 
-memo2taxid={}
-with open(data_dir+'speclist.txt') as fp:
-    for line in fp:
-        if re.match('.*:\sN=',line):
-            s=line.split()
-            taxid=s[2].replace(':','')
-            mnemonic=s[0]
-            memo2taxid[mnemonic]=taxid
+    if ns.gc:
+        type+="_GC"
+        extra+=1
+    if ns.gcgenomic:
+        type+="_GCgenomic"
+        extra+=1
+
+    if ns.kingdom:
+        type+="_kingdom"
+        extra+=3
 
     
-outlist = open('formatted_list','w')
+    # This is GC data for genomes ("we could also just add that in the command line")
+    data_dir = "/home/arnee/git/paper_proks_vs_euks_proteins/data/"
+    df_reference_file = data_dir  + "df_reference.csv"
+    df_reference = pd.read_csv(df_reference_file)
+    taxid2gc = df_reference.set_index("TaxID").to_dict()["GC%"]
+
+    fastafile=ns.fasta
+    dnafile=ns.dna
+    GCgenomic=ns.GCgenomic    
+    try:
+        for record in SeqIO.parse(fastafile, "fasta"):
+            #print("%s %i" % (record.id, len(record)))
+            bar,id,name=record.id.split("|") 
+            #print (record.id,id,name)
+            prot,taxname=name.split("_") 
+            try:
+                GCgenomic=float(taxid2gc[int(memo2taxid[taxname])])
+            except:
+                skipped+=[key]
+                continue
+        for record in SeqIO.parse(fastafile, "fasta"):
+            #print("%s %i" % (record.id, len(record)))
+            bar,id,name=record.id.split("|") 
+            #print (record.id,id,name)
+            prot,taxname=name.split("_") 
+            try:
+                GCgenomic=float(taxid2gc[int(memo2taxid[taxname])])
+            except:
+                skipped+=[key]
+                continue
 
 
-out = h5py.File('formatted_data_'+type+'.h5py', 'w')
-skipped=[]
-for key in data:                                                                ##### key: uniprot/MobiDB ID
-    #print (key)
-    fastafile=fasta_dir+key+".fasta"
+    #------------------------------------------------------------------------            
+    #            try:
+    #                gc=float(taxid2gc[int(tax_id)])
+    
+    
+    fasta_dir="uniprot/"
+    with open(ns.f,'rb') as f:
+        data = pickle.load(f)
+
+    memo2taxid={}
+    with open(data_dir+'speclist.txt') as fp:
+        for line in fp:
+            if re.match('.*:\sN=',line):
+                s=line.split()
+                taxid=s[2].replace(':','')
+                mnemonic=s[0]
+                memo2taxid[mnemonic]=taxid
+                
+    
+    outlist = open('formatted_list','w')
+
+
+    out = h5py.File('formatted_data_'+type+'.h5py', 'w')
+    skipped=[]
+    for key in data:                                                                ##### key: uniprot/MobiDB ID
+        #print (key)
+        fastafile=fasta_dir+key+".fasta"
     GCgenomic=0.0
+
+    
     try:
         for record in SeqIO.parse(fastafile, "fasta"):
             #print("%s %i" % (record.id, len(record)))
@@ -306,4 +299,43 @@ for key in skipped:
 out.close()
 outlist.close()
 skiplist.close()
+
+        
+
+
+        
+    
+    ##### Dataset #####
+    #print ('Loading data ...')
+    #data = h5py.File(ns.d+'formatted_data_GC.h5py','r')
+    #with open(ns.d+'mobidata_K.pickle','rb') as f:
+    #    gc = pickle.load(f)
+    #test_list = []
+    #for line in open(ns.t, 'r'): test_list.append(line.rstrip())
+
+    model = load_model(ns.m)
+
+    test_cm = {}
+    for thr in nl.thrlist: test_cm[thr] = test_cm.get(thr, {'PP':{'TP':0,'FP':0},'PN':{'TN':0,'FN':0}})
+
+# This is not complete as we have to check if we have the data for that residue as well
+    ##### Prediction #####
+    #pred = {'Name':[], 'kingdom':[], 'gc':[], 'TP':[], 'FP':[], 'FN':[], 'TN':[], 'Pred':[], 'Diso':[]}
+    pred = {}
+    i=0
+
+
+    # First we need to take a fasta file (+DNA data) and make h5py file.
+
+
+    for protein in test_list:
+            sample=np.concatenate((data[protein]['GC'],data[protein][ns.f]),axis=1)
+        else:
+            sample = np.array(data[protein][ns.f], dtype=np.float64)
+        X = sample[:,:-1].reshape(1, len(sample), len(sample[0])-1)
+        Y = sample[:,-1]
+        #print (protein,X,Y)
+        prediction = model.predict_on_batch(X)
+
+
 
