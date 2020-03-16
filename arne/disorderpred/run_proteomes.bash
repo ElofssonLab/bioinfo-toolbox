7f1a0774d9e1236a3b952319f779d17d03b8fa2d
Arne Elofsson (ae18c) <arne@bioinfo.se>
2020-03-16 16:43:31 +0100

    By syncing

--- arne/disorderpred/run_proteomes.bash
+++ arne/disorderpred/run_proteomes.bash
@@ -19,17 +19,17 @@ do
     then
 	/opt/singularity3/bin/singularity exec --nv tf_sandbox/ python3 bin/dis_pred.py -rnafile -pro -gc -f $i -m models/model_100-test1234.sorted.GCgenomic-10-0.001_1_pro_GC_16.ann >$i.model_pro_GC
     fi
-    if [ ! -s  $i.model_conv1d_pro_GC] 
+    if [ ! -s  $i.model_conv1d_pro_GC ] 
     then
 	/opt/singularity3/bin/singularity exec --nv tf_sandbox/ python3 bin/dis_pred.py -rnafile -pro -gc -f $i -m models/model_conv1d_100-test1234.sorted.GCgenomic-10-0.001_1_pro_GC_78.ann >$i.model_conv1d_pro_GC
     fi
     # Protein + GCgenomiv
 
-    if [ ! -s  proteomes/$j.fasta.model_pro_GCgenomic    ]
+    if [ ! -s  proteomes/$j.fasta.model_pro_GCgenomic  ]
     then
 	/opt/singularity3/bin/singularity exec --nv tf_sandbox/ python3 bin/dis_pred.py -pro -gcgenomic -f proteomes/$j.fasta -m models/model_100-test1234.sorted.GCgenomic-10-0.001_1_pro_GCgenomic_95.ann >  proteomes/$j.fasta.model_pro_GCgenomic    
     fi
-    if [ ! -s  proteomes/$j.fasta.model_conv1d_pro_GCgenomic    ]
+    if [ ! -s  proteomes/$j.fasta.model_conv1d_pro_GCgenomic  ]
     then
 	/opt/singularity3/bin/singularity exec --nv tf_sandbox/ python3 bin/dis_pred.py -pro -gcgenomic -f proteomes/$j.fasta -m models/model_conv1d_100-test1234.sorted.GCgenomic-10-0.001_1_pro_GCgenomic_9.ann >  proteomes/$j.fasta.model_conv1d_pro_GCgenomic    
     fi
