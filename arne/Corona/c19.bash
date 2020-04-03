1cd0b7c828940ce6ed80e5e50800540bd26b20f9
Arne Elofsson <arne@bioinfo.se>
2020-04-03 11:01:50 +0200

    updated c19

--- arne/Corona/c19.bash
+++ arne/Corona/c19.bash
@@ -6,7 +6,7 @@ dir=~/Desktop/Corona/c19/data/
 
 for i in Stockholm Östergötland "Västra Götaland" Skåne Sörmland Uppsala Jönköping Örebro Dalarna Halland Gävleborg Västmanland Jämtland Norrbotten Västerbotten Västernorrland Värmland Kronoberg Kalmar Blekinge Gotland Okänt
 do
-    curl https://c19.se/Sweden/$i | grep -E "data:|categories:" > $dir/$i.txt
+    curl https://c19.se/Sweden/$i | grep -E "name:|data:|categories:" > $dir/$i.txt
 done
 # datum, Fall, Fall idag, döda, Total IVA
 
--- arne/Corona/c19.py
+++ arne/Corona/c19.py
@@ -17,12 +17,13 @@ inp_dir=home = str(Path.home())+"/Desktop/Corona/c19/data/"
 out_file=home = str(Path.home())+"/Desktop/Corona/c19/data/c19-"+str(today)+".csv"
 sys.stdout = open(out_file, "w")
 
-columns=["country","province","DateRep", "confirmed", "new_confirmed_cases", "deaths", "IntensiveCare"]
+columns=["country","province","DateRep", "confirmed", "new_confirmed_cases", "deaths", "IntensiveCare","Hospitalized"]
+
 country="Sweden"
 alldata=[]
 file_list=[]
-print(f'{columns[0]:s},{columns[1]:s},{columns[2]:s},{columns[3]:s},{columns[4]:s},{columns[5]:s},{columns[6]:s}')
-
+print(f'{columns[0]:s},{columns[1]:s},{columns[2]:s},{columns[3]:s},{columns[4]:s},{columns[5]:s},{columns[6]:s},{columns[7]:s}')
+mapping={"Fall":"confirmed","Fall idag":"new_confirmed_cases","Döda":"deaths","På IVA":"IntensiveCare","På sjukhus":"Hospitalized"}
 for f in os.listdir(inp_dir):
     if (f.find(".txt")!=-1):
         region=re.sub(r'.txt','',f)
@@ -33,13 +34,27 @@ for f in os.listdir(inp_dir):
             lines =  [line.rstrip(']\n').lstrip('categorisdt: [') for line in file]
             for line in lines:
                 #print (row,line)
-                data[row]=line.split(',')
-                row+=1
-            for i in range(len(data[0])):
-                #print(f'{f(x,n):d}, b={g(x,n):d}')
-                d=re.sub(r" '|'","",data[0][i])+" 2020"
+                if line.find("name") !=-1:
+                    foo,type,bar=line.split("'")
+                    #print ("type:",type)
+                else:
+                    if row==0:
+                        datum=line.split(',')
+                    else:
+                        data[mapping[type]]=line.split(',')
+                    row+=1
+            #print (data)
+            for key in mapping.keys():
+                if not mapping[key] in data:
+                    data[mapping[key]]=[]
+                    for i in range(len(datum)):
+                        data[mapping[key]]+=[0]
+            for i in range(len(datum)):
+                d=re.sub(r" '|'","",datum[i])+" 2020"
                 date=datetime.strptime(d,"%b %d %Y").date()
-                print(f'{country:s},{region:s},{date},{int(data[1][i]):d},{int(data[2][i]):d},{int(data[3][i]):d},{int(data[4][i]):d}')
+                print(f'{country:s},{region:s},{date},{int(data["confirmed"][i]):d},{int(data["new_confirmed_cases"][i]):d},{int(data["deaths"][i]):d},{int(data["IntensiveCare"][i]):d},{int(data["Hospitalized"][i]):d}')
+
+                
                 #printf  (country,region,i,data[0][i],data[1][i],data[2][i],data[3][i],data[4][i])
                 #for j in range(len(data[0][0])):
                 #alldata+=[[country],[region],[data[0][i]],[data[1][i]],[data[2][i]],[data[3][i]],[data[4][i]]]
