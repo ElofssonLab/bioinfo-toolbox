26f89eb69f836e729babf4d219a1620928addec9
Arne Elofsson <arne@bioinfo.se>
2020-06-09 08:21:45 +0200

    By syncing

--- arne/docking/mergeSTObyGenome.py
+++ arne/docking/mergeSTObyGenome.py
@@ -6,16 +6,41 @@ import sys, getopt,re
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+import argparse
+from argparse import RawTextHelpFormatter
+        
+##args = docopt.docopt(__doc__)
+#out_dir = args['--output_folder']
+ 
+
+p = argparse.ArgumentParser(description = '- plotting trRosetta maps-',
+                            formatter_class=RawTextHelpFormatter)
+p.add_argument('-fileA','--inputA','-i', required= True, help='Input File A')
+p.add_argument('-fileB','--inputB','-j', required= True, help='Input file B')
+#p.add_argument('-dom','--domain','-d', required= False, help='positions of domain borders', nargs='+')
+p.add_argument('-seq','--sepseq','-s', required= False, help='sequence file to identify domain baorders (>10Ala in a row)')
+p.add_argument('--genus','-g', required= False, help='Use only genus names', action='store_true')
+p.add_argument('--host','-l', required= False, help='use host to match fileB', action='store_true')
+#p.add_argument('-out','--output','-o', required= False, help='output image')
+#parser.add_argument('--nargs', nargs='+')
+ns = p.parse_args()
 
 #from Bio.SeqFeature import SeqFeature, FeatureLocation
 
 sepseq="AAAAAAAAAAAAAAAAAAAA"
 
-fileA=sys.argv[1]
-fileB=sys.argv[2]
+fileA=ns.inputA
+fileB=ns.inputB
 
-use_genus=True  # THis is a flag that we set to only match on Genus (i.e. first word in genus)
-use_host=True # If this is set we use the hostname for virus proteins (from NCBI virues
+if (ns.genus):
+   use_genus=True  # THis is a flag that we set to only match on Genus (i.e. first word in genus)
+else:
+   use_genus=False
+
+if (ns.host):
+   use_host=True # If this is set we use the hostname for virus proteins (from NCBI virues
+else:
+   use_host=False
 
 handleA = open(fileA, 'r')
 
