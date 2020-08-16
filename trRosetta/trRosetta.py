3e263049f09d66a1f5a1e92d13084c7df48038fa
Arne Elofsson <arne@bioinfo.se>
2020-08-16 16:59:14 +0200

    hej

--- trRosetta/trRosetta.py
+++ trRosetta/trRosetta.py
@@ -2,12 +2,45 @@ import sys,os,json
 import tempfile
 import numpy as np
 
-from arguments import *
+#from arguments import *
 from utils_ros import *
 from pyrosetta import *
 from pyrosetta.rosetta.protocols.minimization_packing import MinMover
 
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
+
+import argparse
+
+def get_args(params):
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("NPZ", type=str, help="input distograms and anglegrams (NN predictions)")
+    parser.add_argument("FASTA", type=str, help="input sequence")
+    parser.add_argument("OUT", type=str, help="output model (in PDB format)")
+
+    
+    parser.add_argument('-minprob', type=float, dest='minprob', default=0.5, help='min probability of distance restraints for inter-chain flat harminic')
+    parser.add_argument('-intradist', type=float, dest='intradist', default=15., help='The distance for the flat part in  restraints for inter-chain flat harminic')
+    parser.add_argument('-intrasd', type=float, dest='intrasd', default=20., help='SD for  restraints for inter-chain flat harminic')
+    parser.add_argument('-allintra',  dest='allcontacts', help='Use in additiona also a weak interacting potential for all intrachain contacts',action='store_true')
+    parser.add_argument('-pd', type=float, dest='pcut', default=params['PCUT'], help='min probability of distance restraints')
+    parser.add_argument('-m', type=int, dest='mode', default=2, choices=[0,1,2], help='0: sh+m+l, 1: (sh+m)+l, 2: (sh+m+l)')
+    parser.add_argument('-w', type=str, dest='wdir', default=params['WDIR'], help='folder to store temp files')
+    parser.add_argument('-n', type=int, dest='steps', default=1000, help='number of minimization steps')
+    parser.add_argument('--orient', dest='use_orient', action='store_true', help='use orientations')
+    parser.add_argument('--no-orient', dest='use_orient', action='store_false')
+    parser.add_argument('--fastrelax', dest='fastrelax', action='store_true', help='perform FastRelax')
+    parser.add_argument('--no-fastrelax', dest='fastrelax', action='store_false')
+    parser.set_defaults(use_orient=True)
+    parser.set_defaults(fastrelax=True)
+
+    args = parser.parse_args()
+
+    params['PCUT'] = args.pcut
+    params['USE_ORIENT'] = args.use_orient
+
+    return args
+
 
 def main():
 
@@ -23,7 +56,8 @@ def main():
     # get command line arguments
     args = get_args(params)
     print(args)
-
+    
+    
     # init PyRosetta
     init('-hb_cen_soft -relax:default_repeats 5 -default_max_cycles 200 -out:level 100')
 
