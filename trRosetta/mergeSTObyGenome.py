93274b0d11d74d3118c2347e02548e4bd569f376
Arne Elofsson <arne@bioinfo.se>
2020-09-07 21:47:39 +0200

    update notebook

--- trRosetta/mergeSTObyGenome.py
+++ trRosetta/mergeSTObyGenome.py
@@ -4,11 +4,12 @@ from __future__ import print_function
 import sys, getopt,re
 
 from Bio import SeqIO
+from Bio import AlignIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import argparse
 from argparse import RawTextHelpFormatter
-        
+from Bio.Align import MultipleSeqAlignment        
 ##args = docopt.docopt(__doc__)
 #out_dir = args['--output_folder']
  
@@ -21,6 +22,8 @@ p.add_argument('-fileB','--inputB','-j', required= True, help='Input file B')
 p.add_argument('-seq','--sepseq','-s', required= False, help='sequence file to identify domain baorders (20 Gly)',default="GGGGGGGGGGGGGGGGGGGG")
 p.add_argument('--genus','-g', required= False, help='Use only genus names', action='store_true')
 p.add_argument('--host','-l', required= False, help='use host to match fileB', action='store_true')
+p.add_argument('--skipped','-S', required= False, help='file to print skipped files (endings will be added)')
+p.add_argument('--paralogs','-p', required= False, help='file to print paralogous files (endinfs will be added)')
 #p.add_argument('-out','--output','-o', required= False, help='output image')
 #parser.add_argument('--nargs', nargs='+')
 ns = p.parse_args()
@@ -48,6 +51,16 @@ handleA = open(fileA, 'r')
 
 dataA={}
 dataB={}
+
+
+#skippingA=MultipleSeqAlignment([])
+#skippingB=MultipleSeqAlignment([])
+#paralogA=MultipleSeqAlignment([])
+#paralogB=MultipleSeqAlignment([])
+skippingA=[]
+skippingB=[]
+paralogA=[]
+paralogB=[]
 
 #print ("opening "+ fileA +"\n")
 # For each record 
@@ -81,19 +94,21 @@ for record in SeqIO.parse(handleA, 'stockholm') :
          if use_host:
             organism=host
             #print (organism)
-         #try: 
-         #   geneid,name,host,organism=re.split(r'\|',record.description)
-         #except:
+         try: 
+            geneid,name,host,organism=re.split(r'\|',record.description)
+         except:
          #   print ("skipping: ",record.name)
-         #   continue
+            skippingA+=[record]
+            continue
       if use_genus:
          organism=re.sub(r'\s+','',organism)
       if (not organism in dataA.keys()):
          #print ("A:",organism,(record.name,record.description,organism))
          dataA[organism]=record
+      else:
+         paralogA+=[record]
+
          
-
-
 handleB = open(fileB, 'r')
 #print ("opening "+ fileB +"\n"        )
 first=True
@@ -120,6 +135,7 @@ for record in SeqIO.parse(handleB, 'stockholm') :
             geneid,name,host,organism,region=re.split(r'\|',record.description)
          except:
             #print ("skipping: ",record.name,re.split(r'\|',record.description))
+            skippingB+=[record]
             continue
          if use_host:
             organism=host
@@ -128,8 +144,10 @@ for record in SeqIO.parse(handleB, 'stockholm') :
       if (not organism in dataB.keys()):
          #print ("B",record.name,organism)
          dataB[organism]=record
+      else:
+         paralogB+=[record]
 
-# First we shoudl always use sequecne 1 in both files...
+# First we should always use sequecne 1 in both files...
 
 print ("> " + seqA.name + " AND " + seqB.name)
 print (seqA.seq+sepseq+seqB.seq)
@@ -139,3 +157,14 @@ for key in dataA.keys():
       print ("> " + key )
       print (dataA[key].seq+sepseq+dataB[key].seq)
 
+
+if (ns.skipped):
+   msaA=MultipleSeqAlignment(skippingA)
+   msaB=MultipleSeqAlignment(skippingB)
+   AlignIO.write(msaA, ns.skipped+"-A.fasta", "fasta")
+   AlignIO.write(msaB, ns.skipped+"-B.fasta", "fasta")
+if (ns.paralogs):
+   msaA=MultipleSeqAlignment(paralogA)
+   msaB=MultipleSeqAlignment(paralogB)
+   AlignIO.write(msaA, ns.paralogs+"-A.fasta", "fasta")
+   AlignIO.write(msaB, ns.paralogs+"-B.fasta", "fasta")
