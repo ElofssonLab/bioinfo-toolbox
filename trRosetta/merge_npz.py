dab2b16e23a2d3cea4690ed155b0b555b46cfc2d
Arne Elofsson <arne@bioinfo.se>
2020-09-29 16:32:27 +0200

    fixed merge_npz

--- trRosetta/merge_npz.py
+++ trRosetta/merge_npz.py
@@ -14,7 +14,7 @@ p.add_argument('-dataA','--inputA','-i', required= True, help='Input trRossetta 
 p.add_argument('-dataB','--inputB','-j', required= True, help='Input trRossetta NPZ file')
 p.add_argument('-dataAB','--inputAB','-k', required= True, help='Input trRossetta NPZ file(s) for the mergedsequence',nargs="+")
 #p.add_argument('-seq','--sequence','-s', required= True, help='sequence file to identify domain baorders')
-p.add_argument('-out','--output','-o', required= True, help='output NPX file')
+p.add_argument('-out','--output','-o', required= True, help='output NPZ file')
 #parser.add_argument('--nargs', nargs='+')
 ns = p.parse_args()
 
@@ -25,10 +25,14 @@ bins = np.array([2.25+bin_step*i for i in range(36)])
 
 rstA = np.load(ns.inputA)
 rstB = np.load(ns.inputB)
+#print ("reading ",ns.inputA)
+#print ("reading ",ns.inputB)
 rstAB=[]
 for i in ns.inputAB:
-    rstAB += [np.load(ns.inputAB[0])]
+    #print ("reading ",i)
+    rstAB += [np.load(i)]
 
+#print (rstAB)
 
 
 distA = rstA["dist"].shape[0]
@@ -52,27 +56,39 @@ for f in rstAB[0].files:
     new_rst[f]=rstAB[0][f]
 
 
-    
+            
+#bins = np.array([2.25+bin_step*i for i in range(36)])    
 # Take the one with the lowsest  probability to not have a distance
 # To speed up things we only do this for intrachan
 for d in range(1,len(rstAB)):
+    #print ("Using ",d,distA,distB)
     for i in range(distA):
-        for j in range(distA+1,distB):
+        for j in range(distA+1,distA+distB):
             orgprob = new_rst["dist"][i, j, 0]
             newprob = rstAB[d]["dist"][i, j, 0]
             if newprob < orgprob:
+                #if (newprob < 0.5 or orgprob < 0.5):
+                #    print (i,j,d,newprob,orgprob)
+                #    d_slice = new_rst["dist"][i, j, 1:]
+                #    mean_dist = np.sum(np.multiply(bins, d_slice/np.sum(d_slice)))
+                #    print ("orgdist",mean_dist)
+                #    d_slice = rstAB[d]["dist"][i, j, 1:]
+                #    mean_dist = np.sum(np.multiply(bins, d_slice/np.sum(d_slice)))
+                #    print ("newdist",mean_dist,d)
                 for f in rstAB[0].files:
                     new_rst[f][i,j]=rstAB[d][f][i,j]
                     new_rst[f][j,i]=rstAB[d][f][j,i]
 
                     
-# Then we add the     
+# Then we add the  individual proteins   
 for i in rstAB[0].files:
     A=rstA[i]
     B=rstB[i]
-    AB=rstAB[0][i]
+    AB=new_rst[i]
     AB[0:distA,0:distA]=A    
     AB[distA:,distA:]=B    
     new_rst[i]=AB
+
+
 
 np.savez_compressed(ns.output, dist=new_rst['dist'], omega=new_rst['omega'], theta=new_rst['theta'], phi=new_rst['phi']) #, rep=new_rst['rep'])
