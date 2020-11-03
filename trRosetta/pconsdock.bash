4d57388c01bf535efbad21e239d0a9473228dfbf
Arne Elofsson (ae18c) <arne@bioinfo.se>
2020-11-03 16:03:03 +0100

    added local version of dockQ!

--- trRosetta/pconsdock.bash
+++ trRosetta/pconsdock.bash
@@ -10,6 +10,6 @@ do
     dir=`dirname $i `
     for j in $@
     do
-	python3 ~/git/DockQ/DockQ.py -short $i $j
+	python3 ~/git/bioinfo-toolbox/DockQ/DockQ.py -short $i $j
     done | gawk '{if ($2<1) {i+=1;s+=$2}}END{print i,s,s/i}' > ${dir}/${name}.pconsdock
 done
