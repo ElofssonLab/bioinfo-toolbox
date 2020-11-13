13d6d8a33bff8a74cc3c66cee588b898d6e6c907
gabrielepozzati <gabrielepozzati@hotmail.it>
2020-11-13 10:04:52 +0100

    -

--- trRosetta/contact_prcurve.py
+++ trRosetta/contact_prcurve.py
@@ -8,6 +8,10 @@ from Bio.PDB import *
 import matplotlib.pyplot as plt
 from sklearn.metrics import auc
 from scipy.stats import pearsonr
+import seaborn as sb
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 def get_sep(seq1):
 
@@ -43,18 +47,13 @@ def prob_to_dist(distpred):
     raw_predictions = distpred[:,:,1:nbins+1]
     binary_predictions = distpred[:,:,0]
 
-    no_contact_prob = distpred[:,:,0]
-    contact_prob = np.expand_dims(np.sum(raw_predictions, axis=-1), axis=-1)
-    contact_split = np.subtract(contact_prob, no_contact_prob)
+    contact_prob = np.sum(raw_predictions, axis=-1)
 
-    norm_predictions = np.divide(raw_predictions, contact_prob)
+    norm_predictions = np.divide(raw_predictions, contact_prob[:,:,None])
     values = np.multiply(norm_predictions, bins[None, None, :])
-    mean_values = np.expand_dims(np.sum(values, axis=-1), axis=-1)
-    mean_values[np.where(contact_split<=0)] == 21
+    mean_values = np.sum(values, axis=-1)
 
-    data = np.concatenate((contact_prob, mean_values), axis=-1)
-
-    return data
+    return mean_values
 
 def interface_contacts(code1, code2):
 
@@ -86,50 +85,50 @@ def interface_contacts(code1, code2):
             d = C1-C2
             for pos, thr in enumerate(bins):
                 if d <= thr: 
-                    cmap[pos1][pos2][pos] = 1.0  
+                    cmap[pos1,pos2,pos] = 1.0  
                     break
-            if np.all(cmap[pos1][pos2]==0.0): 
-                cmap[pos1][pos2][0] = 1.0
-
+            if np.all(cmap[pos1,pos2]==0.0): 
+                cmap[pos1,pos2,0] = 1.0
+                cmap[pos1,pos2,-1] = 1.0
     return cmap
 
 def sort_pred(pred):
     sorted_list = []
     for px, x in enumerate(pred):
         for py, y in enumerate(x):
-            prob = np.max(y)
+            prob = np.max(y[1:])
             if sorted_list == []:
                 sorted_list.append([px,py,prob])
                 continue
             for p, el in enumerate(sorted_list):
-                if prob > el[2]: sorted_list = sorted_list[:p]+[[px,py,prob]]+sorted_list[p:]
-                break
+                if prob > el[2]: 
+                    sorted_list = sorted_list[:p]+[[px,py,prob]]+sorted_list[p:]
+                    break
             if [px,py,prob] not in sorted_list: sorted_list.append([px,py,prob])
 
     return sorted_list
 
-def bin_auc(real, pred, sortp, tol):
+def bin_auc(pred, real, sortp, tol, int_bins):
 
     TPRs = []
     PPVs = []
     TP = TN = FP = FN = 0
     for x, y, p in sortp:
-        realbin = np.argmax(real[x][y])
-        if max(real[x][y][realbin], real[x][y][0]) != real[x][y][0]: 
-            tolrange = [el for el in range(realbin-tol, realbin+tol+1) if el > 0]
-        else: tolrange = [0]+[el for el in range(36, 36-(tol+1), -1)]
-
-        if np.argmax(pred[x][y]) in tolrange: FN += 1
+        realbin = np.argmax(real[x,y,1:])
+        predbin = np.argmax(pred[x,y,1:])
+        if realbin > int_bins and predbin > int_bins: continue
+        tolrange = [el for el in range(realbin-tol, realbin+tol+1) if el > 0 and el <= 35]
+        if predbin in tolrange: FN += 1
         else: TN += 1
-   
+    
     prev = 1.1
     for x, y, p in sortp:
-        realbin = np.argmax(real[x][y])
-        if max(real[x][y][realbin], real[x][y][0]) != real[x][y][0]:
-            tolrange = [el for el in range(realbin-tol, realbin+tol+1) if el > 0]
-        else: tolrange = [0]+[el for el in range(36, 36-(tol+1), -1)]
+        realbin = np.argmax(real[x,y,1:])
+        predbin = np.argmax(pred[x,y,1:])
+        if realbin > int_bins and predbin > int_bins: continue
+        tolrange = [el for el in range(realbin-tol, realbin+tol+1) if el > 0 and el <= 35]
 
-        if np.argmax(pred[x][y]) in tolrange:
+        if predbin in tolrange:
             FN -= 1
             TP += 1
         else:
@@ -137,61 +136,73 @@ def bin_auc(real, pred, sortp, tol):
             TN -= 1
 
         if p != prev:
-            if TP != 0 or FN != 0: TPRs += [TP/(TP+FN)]
-            else: TPRs += [0.0]
-            if TP != 0 or FP != 0: PPVs += [TP/(TP+FP)]
-            else: PPVs += [0.0]
-
+            if (TP != 0 or FN != 0) and (TP != 0 or FP != 0): 
+                TPRs += [TP/(TP+FN)]
+                PPVs += [TP/(TP+FP)]
         prev = p
 
-    if TP != 0 or FN != 0: TPRs += [TP/(TP+FN)]
-    else: TPRs += [0.0]
-    if TP != 0 or FP != 0: PPVs += [TP/(TP+FP)]
-    else: PPVs += [0.0]
+    if (TP != 0 or FN != 0) and (TP != 0 or FP != 0): 
+        TPRs += [TP/(TP+FN)]
+        PPVs += [TP/(TP+FP)]
 
-    return auc(TPRs, PPVs)
+    #print (PPVs, TPRs)
+    df = {'ppv':PPVs, 'tpr':TPRs}
+    df = pd.DataFrame(df)
+    #print (df)
+    sb.lineplot(x='tpr', y='ppv', data=df)
+    plt.show()
+    
+    if len(TPRs) < 2 or len(PPVs) < 2: return 0.0
+    else: return auc(TPRs, PPVs)
 
 
 if __name__ == "__main__":
 
     p = argparse.ArgumentParser(description = '- Plot PPV stats for a Cmap or a list of Cmaps')
-    p.add_argument('-l', required= False, default=None, help='codes list (codes must be identical for all files)')
     p.add_argument('-c', required= True, help='trRosetta file with .npz format (folder path only if code list is specified)')
     p.add_argument('-s1', required= True, help='structure file 1 with .pdb format (folder path only if code list is specified)')
     p.add_argument('-s2', required= True, help='structure file 2 with .pdb format (folder path only if code list is specified)')
     ns = p.parse_args()
 
-    if ns.l is None: runlist = ['']
-    else: runlist = [code.rstrip() for code in open(ns.l)]
+    aucs12 = aucs8 = {}
+    tolrange = range(2, 8, 2)
+    for t in tolrange: aucs12[t] = 0
+    for t in tolrange: aucs8[t] = 0
+
+    header = '#\t\t\t'
+    for t in tolrange: header += 'Int.12A-Tol.{}\t'.format((t*0.5)+0.5)
+    for t in tolrange: header += 'Int.8A-Tol.{}\t'.format((t*0.5)+0.5)
+    print (header)
+
+    str1 = ns.s1
+    str2 = ns.s2
+    npz = ns.c
+
+    #Extract the interface contacts from the real structures
+    real_cmap = interface_contacts(str1, str2)
+    sep = len(get_sep(str1)) 
+
+    #Extract the predicted inter-protein contacts
+    with np.load(npz) as npz_file: pred_cmap = npz_file['dist'][:sep, sep:, :]
     
-    aucs = {}
-    for t in range(0, 6, 2): aucs[t] = 0
+    assert real_cmap.shape == pred_cmap.shape,\
+           'Cmaps doesn\'t match, real - {} vs pred - {}'.format(real_cmap.shape, pred_cmap.shape)
 
-    for code in runlist:
-        if code == '':
-            str1 = ns.s1
-            str2 = ns.s2
-            npz = ns.c
-        else: 
-            str1 = ns.s1+code+'.pdb'
-            str2 = ns.s2+code+'.pdb'
-            npz = ns.c+code+'.npz'
+    sorted_list = sort_pred(pred_cmap)
 
-        #Extract the interface contacts from the real structures
-        real_cmap = interface_contacts(str1, str2)
-        sep = len(get_sep(str1)) 
+    codeauc8 = []
+    codeauc12 = []
+    for t in tolrange: 
+        aucval12 = bin_auc(pred_cmap, real_cmap, sorted_list, t, 35)
+        aucs12[t] += aucval12
+        codeauc12.append(aucval12)
+    for t in tolrange: 
+        aucval8 = bin_auc(pred_cmap, real_cmap, sorted_list, t, 12)
+        aucs8[t] += aucval8
+        codeauc8.append(aucval8)
 
-        #Extract the predicted inter-protein contacts
-        with np.load(npz) as npz_file: pred_cmap = npz_file['dist'][:sep, sep:, :]
-        #pdmap = npz_to_casp(pred_cmap)
-    
-        if real_cmap.shape != pred_cmap.shape:
-            print ('Cmaps doesn\'t match, real - {} vs pred - {}'.format(real_cmap.shape, pred_cmap.shape))
-            continue
+    values = ns.c.split('/')[-1].rstrip('.npz')+'\t'
+    for aucval in codeauc12: values += '{}\t\t'.format(round(aucval, 3))
+    for aucval in codeauc8: values += '{}\t\t'.format(round(aucval, 3))
+    print (values)
 
-        sorted_list = sort_pred(pred_cmap)
-        for t in range(0, 6, 2): aucs[t] += bin_auc(pred_cmap, real_cmap, sorted_list, t)
-        
-    for t in range(0, 6, 2): print ('AUC (tolerance of {}A): {}'.format(t*0.5, aucs[t]/len(runlist)))
-
-    
