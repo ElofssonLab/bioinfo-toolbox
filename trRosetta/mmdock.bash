b838f88bfeb1ebf2ad8a5b7b86229eab1402ed0b
Arne Elofsson (ae18c) <arne@bioinfo.se>
2020-11-13 08:43:50 +0100

    By syncing

--- trRosetta/mmdock.bash
+++ trRosetta/mmdock.bash
@@ -1,5 +1,4 @@
 #!/bin/bash 
-
 MM="/home/arnee/git/bioinfo-toolbox/trRosetta/MMalign"
 
 for i in $@
@@ -9,5 +8,5 @@ do
     for j in $@
     do
 	$MM $i $j | grep TM-score | head -1
-    done | gawk '{if ($2<1) {i+=1;s+=$2}}END{print i,s,s/i}' > ${dir}/${name}.MMdock
+    done  | gawk 'BEGIN{s=0;i=0.00000000001}{if ($2<1) {s+=$2}{i+=1}}END{print i,s,s/i}' > ${dir}/${name}.MMdock
 done
