219d10b8f925f7f888de8f3e444ff3c894f29812
Arne Elofsson <arne@bioinfo.se>
2020-11-18 10:13:15 +0100

    Changed translation to use it correctly

--- trRosetta/translatepdb.py
+++ trRosetta/translatepdb.py
@@ -4,7 +4,7 @@ from Bio.PDB.vectors import rotaxis, calc_angle, calc_dihedral
 from Bio.PDB.Polypeptide import is_aa
 from math import pi
 import math
-import numpy 
+import numpy  as np
 from Bio.PDB import PDBIO
 
 # This code is part of the Biopython distribution and governed by its
@@ -95,39 +95,25 @@ if __name__ == "__main__":
 
     
     if (args.rotation):
-        rotation_matrix = numpy.array((
+        rotation_matrix = np.array((
             (args.rotation[3],args.rotation[4],args.rotation[5]),
             (args.rotation[6],args.rotation[7],args.rotation[8]),
             (args.rotation[9],args.rotation[10],args.rotation[11])
         ),'f')
-        translation_matrix = numpy.array((args.rotation[0],args.rotation[1],args.rotation[2]),'f')
-        for atom in structure.get_atoms():
-            atom.transform(rotation_matrix2, translation_matrix)
+        translation_matrix = np.array((args.rotation[0],args.rotation[1],args.rotation[2]),'f')
+        structure.transform(rotation_matrix, translation_matrix)
     elif (args.angles):
         print ("Input",args.angles)
         alpha=args.angles[0]*pi/180
         beta=args.angles[1]*pi/180
         gamma=args.angles[2]*pi/180
-        #u(1,1)=cosg*cosa
-        #u(1,2)=cosg*sina
-        #u(1,3)=-sing
-        #u(2,1)=sinb*sing*cosa-cosb*sina
-        #u(2,2)=sinb*sing*sina+cosb*cosa
-        #u(2,3)=cosg*sinb
-        #u(3,1)=cosb*sing*cosa+sinb*sina
-        #u(3,2)=cosb*sing*sina-sinb*cosa
-        #u(3,3)=cosg*cosb
-        #from Bio import Struct
-        #from Bio.Struct.Geometry import center_of_mass
         
         for chain in structure[0]:
-            # Center of mass is only present in biopython 1.79 or later
-            #com=chain.center_of_mass()
             ca_atoms = [atom for atom in chain.get_atoms() if atom.name=="CA"]
             com=center_of_mass(ca_atoms)
             #print (com)
             break
-        rotation_matrix=numpy.array((
+        rotation_matrix=np.array((
             (math.cos(alpha)*math.cos(gamma),
              math.sin(alpha)*math.cos(gamma),
              -1*math.sin(gamma)),
@@ -138,7 +124,7 @@ if __name__ == "__main__":
              math.sin(alpha)*math.cos(beta)*math.sin(gamma)-math.sin(beta)*math.cos(alpha),
              math.cos(gamma)*math.cos(beta))
         ),'f')
-        #rotation_matrix2=numpy.array((
+        #rotation_matrix2=np.array((
         #    (math.cos(alpha)*math.cos(beta),
         #     math.cos(alpha)*math.sin(beta)*math.sin(gamma)-math.sin(alpha)*math.cos(gamma),
         #     math.cos(alpha)*math.sin(beta)*math.cos(gamma)+math.sin(alpha)*math.sin(gamma)),
@@ -150,24 +136,30 @@ if __name__ == "__main__":
         #     math.cos(beta)*math.cos(gamma))
         #),'f')
         print ("center of mass",com)
-        translation_matrix = numpy.array((
+        translation_matrix = np.array((
             args.angles[3]+com[0]-rotation_matrix[0,0]*com[0]-rotation_matrix[0,1]*com[1]-rotation_matrix[0,2]*com[2],
             args.angles[4]+com[1]-rotation_matrix[1,0]*com[0]-rotation_matrix[1,1]*com[1]-rotation_matrix[1,2]*com[2],
             args.angles[5]+com[2]-rotation_matrix[2,0]*com[0]-rotation_matrix[2,1]*com[1]-rotation_matrix[2,2]*com[2]
         ),'f')
         pos=[0,0,0]
-        for atom in structure.get_atoms():
-            cord=atom.get_vector()
-            pos=[0,0,0]
-            #print ("test-org",cord)
-            for i in range(3):
-                pos[i]=translation_matrix[i]+rotation_matrix[i,0]*cord[0]+rotation_matrix[i,1]*cord[1]+rotation_matrix[i,2]*cord[2]
-                #print (i,cord[i],pos[i])
-            atom.set_coord(pos)
-            #atom.transform(rotation_matrix2, translation_matrix)
-            #print ("test-rot",atom.get_vector())
+        
+        #for atom in structure.get_atoms():
+        #    cord=atom.get_vector()
+        #    pos=[0,0,0]
+        #    #print ("test-org",cord)
+        #    for i in range(3):
+        #        pos[i]=translation_matrix[i]+rotation_matrix[i,0]*cord[0]+rotation_matrix[i,1]*cord[1]+rotation_matrix[i,2]*cord[2]
+        #        #print (i,cord[i],pos[i])
+        #    #atom.transform(rotation_matrix2, translation_matrix)
+        #    #print ("test-rot",atom.get_vector())
+        #    print (atom.coord)
+        #    atom.coord = np.dot(atom.coord, rotation_matrix.T) + translation_matrix
+        #    print (atom,atom.coord,pos)
+        #    atom.set_coord(pos)
+        #for atom in structure.get_atoms():
+        structure.transform(rotation_matrix.T, translation_matrix)
 
-
+            
         #t(1)=tr1+xligcm-u(1,1)*xligcm-u(1,2)*yligcm-u(1,3)*zligcm
         #t(2)=tr2+yligcm-u(2,1)*xligcm-u(2,2)*yligcm-u(2,3)*zligcm
         #t(3)=tr3+zligcm-u(3,1)*xligcm-u(3,2)*yligcm-u(3,3)*zligcm
