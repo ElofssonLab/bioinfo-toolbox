99d077e3fe335b575f77465198311f0eaa6cc781
Arne Elofsson (ae18c) <arne@bioinfo.se>
2020-11-25 18:06:14 +0100

    By syncing

--- trRosetta/run_pycondock.py
+++ trRosetta/run_pycondock.py
@@ -9,5 +9,5 @@ fa2_file = sys.argv[2]
 rr_file = sys.argv[3]
 out_folder = sys.argv[4]
 
-pyconsFold.model_dock(fa1_file,fa2_file, out_folder, rr=rr_file)
+pyconsFold.model_dock(fa1_file,fa2_file, rr_file, out_folder, rr_pthres=0.5)
 
