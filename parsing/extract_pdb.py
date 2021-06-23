7464f3fb8a1848a8aa0936cc5b00f1fd165d79aa
Arne Elofsson (ae18c) <arne@bioinfo.se>
2021-06-23 09:59:13 +0200

    By syncing

--- parsing/extract_pdb.py
+++ parsing/extract_pdb.py
@@ -26,24 +26,52 @@ structure = p.get_structure('', ns.pdb)
 
 code=ns.pdb.replace(".pdb","_")
 
+three2one = {'ALA':'A','ARG':'R','ASN':'N','ASP':'D',
+             'CYS':'C','GLN':'Q','GLU':'E','GLY':'G',
+             'HIS':'H','ILE':'I','LEU':'L','LYS':'K',
+             'MET':'M','PHE':'F','PRO':'P','SER':'S',
+             'THR':'T','TRP':'W','TYR':'Y','VAL':'V',
+             'MSE':'M'}
+
+
 class NotDisordered(Select):
     def accept_atom(self, atom):
         return not atom.is_disordered() or atom.get_altloc() == "A"
+
+class NotAlt(Select):
+    def accept_atom(self, atom):
+        return not atom.get_altloc() == "A"
+
+class NotHet(Select):
+    def accept_res(self, res):
+        return not res.get_resname()[0:1] == "H_"
     
 for chain in structure[0]: 
-    print (chain,len(chain)) 
+    print (chain,len(chain))
+    
     io=PDBIO() 
     io.set_structure(chain) 
-    io.save(code+chain.id+".pdb", select=NotDisordered())
+    io.save(code+chain.id+".pdb", select=NotAlt())
+    seq=Seq('')
+    for s in chain.get_residues():
+        #print (s,s.get_full_id()[3][0])
+        if s.get_full_id()[3][0]==' ':
+            seq+=three2one[s.get_resname()]
+    print (seq)
+    sequence=Seq(seq)
+    sequence.id=code+chain.id
     # Using CA-CA 
     ppb = CaPPBuilder() 
-    for pp in ppb.build_peptides(chain): 
-        seq=Seq(pp.get_sequence())
-        seq.id=code+chain.id
-        #print (seq,seq.id)
-        #with open(code+chain.id+".fasta", "w") as output_handle:
-        #    SeqIO.write(seq, output_handle, "fasta")
-        output_handle=open(code+chain.id+".fasta", "w")
-        output_handle.write(">"+code+chain.id+"\n")
-        output_handle.write(str(seq)+"\n")
-        output_handle.close()
+    #with open(code+chain.id+".fasta", "w") as output_handle:
+    #    SeqIO.write(seq, output_handle, "fasta")
+
+    #for pp in ppb.build_peptides(chain): 
+    #    seq=Seq(pp.get_sequence())
+    #    seq.id=code+chain.id
+    #    print (seq,seq.id)
+    #    #with open(code+chain.id+".fasta", "w") as output_handle:
+    #    #    SeqIO.write(seq, output_handle, "fasta")
+    output_handle=open(code+chain.id+".fasta", "w")
+    output_handle.write(">"+code+chain.id+"\n")
+    output_handle.write(str(seq)+"\n")
+    output_handle.close()
