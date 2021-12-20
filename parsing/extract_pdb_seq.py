37c183f7ea0f20d2d88de4e6ab480a492d167560
Arne Elofsson (ae18c) <arne@bioinfo.se>
2021-12-20 17:10:14 +0100

    By syncing

---
+++ parsing/extract_pdb_seq.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from parse_pdb_resid import *
+import sys
+import operator
+import numpy as np
+from collections import defaultdict
+import gzip
+
+filename=sys.argv[1]
+if filename.endswith(".gz"):
+    pdbfile = gzip.open(sys.argv[1], 'rb')
+else:
+    pdbfile = open(sys.argv[1], 'r')
+
+code = get_acc(pdbfile)
+if (code == 'XXXX' or code == '' or code == 'xxxx' or code == '    '):
+    code=sys.argv[1]
+pdbfile.close()
+if filename.endswith(".gz"):
+    pdbfile = gzip.open(sys.argv[1], 'rb')
+else:
+    pdbfile = open(sys.argv[1], 'r')
+name = sys.argv[1].replace(".pdb","")
+try:
+    chain = sys.argv[2]
+except:
+    chains = get_all_chains(pdbfile)
+else:
+    chains = chain
+print (":"+code+":")
+print (pdbfile)
+print (chains)
+pdbfile.close()
+#print chains
+for chain in chains:
+    print (code, chain)
+    if filename.endswith(".gz"):
+        pdbfile = gzip.open(sys.argv[1], 'r')
+    else:
+        pdbfile = open(sys.argv[1], 'r')
+    outname=name+".fasta"
+    print (outname)
+    outfile = open(outname,'w')
+    outfile.write("> " + code + "\n")
+    outfile.write(get_atom_seq(pdbfile, chain) + "\n")
+    pdbfile.close()
+    outfile.close()
+#pdbfile = open(sys.argv[1], 'r')
+#print get_coordinates(pdbfile)
+#pdbfile.close()
+
+                                
